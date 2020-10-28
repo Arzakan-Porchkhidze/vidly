ec608e0dab4446ce9ed11aaae84b75559c2b03ef
Arzakan <arzakanporchkhidze17@mgail.com>
2020-10-28 17:57:26 +0400

    adding Unit and Integration tests

--- routes/customers.js
+++ routes/customers.js
@@ -10,14 +10,12 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async(req, res) => {
-    try{
-        const customer = await Customer.findById(req.params.id);
-        res.send(customer);
-    }
-    catch {
-        return res.status(404).send('The customer with the given ID was not found.');
-    }
-        
+
+    const customer = await Customer.findById(req.params.id);
+
+    if(!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+    res.send(customer);
   });
 
 router.post('/', async(req, res) => {
@@ -36,29 +34,25 @@ router.post('/', async(req, res) => {
 router.put('/:id', async(req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    try{
-        const customer = await Customer.findByIdAndUpdate(req.params.id,
-            { 
-              name: req.body.name,
-              isGold: req.body.isGold,
-              phone: req.body.phone
-            }, { new: true });
-        res.send(customer);
-    }
-    catch{
-         return res.status(404).send("The customer with the given ID was not found.")
-    }
+
+    const customer = await Customer.findByIdAndUpdate(req.params.id,
+        { 
+            name: req.body.name,
+            isGold: req.body.isGold,
+            phone: req.body.phone
+        }, { new: true }
+    );
+    if(!customer) return res.status(404).send("The customer with the given ID was not found.");
+
+    res.send(customer);
 });
 
 router.delete('/:id', async(req, res) => {
-    try{
-        const customer = await Customer.findByIdAndRemove(req.params.id);
-        res.send(customer);
-    }
-    catch{
-        return res.status(404).send('The customer with the given ID was not found.');
-    } 
 
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+    if(!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+    res.send(customer);
 });
 
 module.exports = router;
--- routes/rentals.js
+++ routes/rentals.js
@@ -11,50 +11,44 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async(req, res) => {
-    try{
-        const rental = await Rental.findById(req.params.id);
-        res.send(rental);
-    }
-    catch {
-        return res.status(404).send('The rental with the given ID was not found.');
-    }
-        
+
+    const rental = await Rental.findById(req.params.id);
+    if(!rental) return res.status(404).send('The rental with the given ID was not found.');
+
+    res.send(rental);  
 });
 
 router.post('/', async(req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    try{
-        const customer = await Customer.findById(req.body.customerId);
-        try{
-            const movie = await Movie.findById(req.body.movieId);
-            if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
 
-            let rental = new Rental({ 
-                customer: {
-                  _id: customer._id,
-                  name: customer.name, 
-                  phone: customer.phone
-                },
-                movie: {
-                  _id: movie._id,
-                  title: movie.title,
-                  dailyRentalRate: movie.dailyRentalRate
-                }
-              });
-              rental = await rental.save();
+    const customer = await Customer.findById(req.body.customerId);
+    if(!customer) return res.status(400).send('Invalid customer.');
+
+    const movie = await Movie.findById(req.body.movieId);
+    if(!movie) return res.status(400).send('Invalid movie.');
+    
+    if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
+
+        let rental = new Rental({ 
+            customer: {
+                _id: customer._id,
+                name: customer.name, 
+                phone: customer.phone
+            },
+            movie: {
+                _id: movie._id,
+                title: movie.title,
+                dailyRentalRate: movie.dailyRentalRate
+            }
+        });
+    rental = await rental.save();
             
-              movie.numberInStock--;
-              movie.save();
+    movie.numberInStock--;
+    movie.save();
               
-              res.send(rental);
-        }catch{
-            return res.status(400).send('Invalid movie.');
-        }
-    }catch{
-        return res.status(400).send('Invalid customer.');
-    }
+    res.send(rental);
 });
 
 module.exports = router;
